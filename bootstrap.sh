6b7a2df2cbeff7b178a0ec6f7cc97e6cb1e7620f
Angel Garcia <azmavet@gmail.com>
2016-02-11 02:36:48 -0700

    Make vagrant bootstrap shell script more idempotent

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,6 +19,13 @@ else
     apt-get update
     apt-get install -y puppet
 fi
+# Check puppet is installed correctly
+if [ $(command -v puppet) >/dev/null 2>&1 ]; then
+    echo "Puppet installed correctly"
+else
+    echo "Puppet not installed correctly"
+    exit 1
+fi
 
 # Install git
 if [ $(command -v git) >/dev/null 2>&1 ]; then
@@ -30,37 +37,42 @@ else
     fi
     apt-get install -y git
 fi
+# Check git is installed correctly
+if [ $(command -v git) >/dev/null 2>&1 ]; then
+    echo "Git installed correctly"
+else
+    echo "Git not installed correctly"
+    exit 1
+fi
 
 echo "Make root site dir"
-mkdir -p /var/www/puppet/html
+www_root='/var/www/puppet/html'
+mkdir -p $www_root
+if [ ! -d $www_root ]; then
+    echo "Can't make target directy $www_root"
+    exit 1
+fi
 
 echo "Clone site repo"
-if [ ! -d ~/html ]; then
-    git clone https://github.com/puppetlabs/exercise-webpage ~/html
+repo_destination=~/html
+if [ ! -d $repo_destination ]; then
+    git clone https://github.com/puppetlabs/exercise-webpage $repo_destination
 else
-    cd ~/html
+    cd $repo_destination
     git pull
 fi
-if [ $(find ~/html -type f -iname "index.*" | wc -l) -gt 0 ]; then
+if [ $(find $repo_destination -type f -iname "index.*" | wc -l) -gt 0 ]; then
     echo "Github repo cloned/pulled correctly"
 else
     echo "Error pulling github repo"
     exit 1
 fi
-rsync -r --exclude=".*"  ~/html/ /var/www/puppet/html
 
-# Check everything is configured correctly
-if [ $(command -v puppet) >/dev/null 2>&1 ]; then
-    echo "Puppet installed correctly"
-else
-    echo "Puppet not installed correctly"
-    exit 1
-fi
-
-if [ $(command -v git) >/dev/null 2>&1 ]; then
-    echo "Git installed correctly"
-else
-    echo "Git not installed correctly"
+# Copy github repo to target
+repo_destination="$repo_destination/"
+rsync -r --exclude=".*"  $repo_destination $www_root
+if [ $(find $www_root -type f -iname "index.*" | wc -l) -eq 0 ]; then
+    echo "Error copying repo contents to target dir"
     exit 1
 fi
 
